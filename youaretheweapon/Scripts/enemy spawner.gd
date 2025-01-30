092c4fe9378e066cdd9ec9f919714464b01d0ff3
Aalsahsah <127888601+Aalsahsah@users.noreply.github.com>
2025-01-29 22:43:09 -0500

    Final touches on weapon scene, although the spawner and timer no longer work for some reason

--- youaretheweapon/Scripts/enemy spawner.gd
+++ youaretheweapon/Scripts/enemy spawner.gd
@@ -1,16 +1,29 @@
 extends Node
 
 @export var spawn_object = preload("res://Scenes/enemy.tscn").instantiate()
-# Called when the node enters the scene tree for the first time.
+@export var is_alive = "res://Scripts/enemy.gd"
+var failcounter: int = 0
+
 func _ready():
 	spawn() 
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	print_debug(failcounter)
+	if failcounter == 3:
+		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 	pass
 	
 
 func spawn():
 	var obj = spawn_object
 	add_child(obj)
+
+
+func _on_fail_timer_timeout():
+	failcounter += failcounter
+	spawn()
+	pass # Replace with function body.
--- youaretheweapon/Scripts/enemy.gd
+++ youaretheweapon/Scripts/enemy.gd
@@ -1,21 +1,20 @@
 extends Area2D
 var mouse_inside_area = false
-
+var is_alive = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
+func _process(_delta: float):
 	if Input.is_action_pressed("Left Mouse") and mouse_inside_area:
 		$".".queue_free()
+		is_alive = false
 
 func _on_mouse_entered():
 	#pass # Replace with function body.
 	mouse_inside_area = true
 
 func _on_mouse_exited():
-	# by leaving pass before the rest of the function, it will essentially return without doing anything
-	#pass # Replace with function body.
 	mouse_inside_area = false
